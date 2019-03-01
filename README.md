1dd657baa23af317cd1f3844517acf392cb8c1d9
Irina Guberman <irina.guberman@xaptum.com>
2019-02-28 18:43:32 -0600

    Initial commit with DEMO 1 and DEMO 2

--- README.md
+++ README.md
@@ -1,23 +1,46 @@
 # erljuggler_demo
 A demo of a uniquely resilient simulated "server" made possible by unique features of Erlang VM, the BEAM
 
+```
 cd src
 erl
+```
 
-DEMO 1 JUGGLER:
+## DEMO 1 JUGGLER:
+
+```
 >c(juggler).
+
 >juggler:accept(10000). %% 10,000 = NumRequests we'll handle
+
 > %% (of them every Nth request will result in inf loop) can be modified by changing define of BUG_MOD
-**** 9620 [INF]***** %%% INDICATES INFINITE LOOP WAS SPAWNED by request # 9620
-...9392 [3090]......9393 [2083]    %%% INDICATES a "request_handler" process was finished in [millis], in this case request #9392 finished in 3030 millis and request # 9393 finished in 2083 millis. 
+**** 9620 [INF]*****
+```
+
+_INDICATES INFINITE LOOP WAS SPAWNED by request # 9620_
+
+```
+...9392 [3090]......9393 [2083]
+```
+
+_INDICATES a "request_handler" process was finished in `[millis]`,
+in this case request #9392 finished in 3090 millis and request # 9393 finished in 2083 millis_
 . 
 .
 .
->9000 finished, 1000 running forever %%% 9000 requests successfully completed, while 1000 are inf loops that will be running until you kill the shell 
+
+```
+9000 finished, 1000 running forever
+
 ok
->%%% Feel free to try again, but now we'll be running 2000 inf loops, the rest of the requests will still be completing
+```
+
+_TELLS us that 1000 processes are still running, so be careful, your entire system might be affected_
 
 
-DEMO 2 KILLER JUGGLER:   %% Same as JUGGLER, but much more useful!
+## DEMO 2 KILLER JUGGLER:
+#### Same as JUGGLER, but much more useful!
+```
 >c(killer_juggler).
->killer_juggler:accept(10000). 
+>killer_juggler:accept(10000).
+```
---
+++ src/killer_juggler.erl
@@ -0,0 +1,135 @@
+%%%-------------------------------------------------------------------
+%%% @author iguberman
+%%% @doc
+%%%
+%%% @end
+%%% Created : 27. Feb 2019 7:27 PM
+%%%-------------------------------------------------------------------
+-module(killer_juggler).
+-author("iguberman").
+
+-define(BUG_MOD, 10).
+
+%% API
+-export([
+  accept_loop/0,
+  accept/1,
+  process_request/2,
+  handle_request/2,
+  dispatch/2,
+  process_stats/0]).
+
+-define(DONT_WAIT_FOR_ME, dont_wait_for_me).
+
+%%% NOT USED For DEMO
+accept_loop() ->
+  Self = self(),
+  receive
+    Req -> dispatch(Req, Self)
+  end,
+  accept_loop().
+
+%% in place of accept loop
+accept(NumRequests)->
+  init_inf_count(),
+  Self = self(),
+  [dispatch(X, Self) || X <- lists:seq(1, NumRequests)],
+  collect_responses(0, 0, 0, 0, NumRequests).
+
+init_inf_count()->
+  case get(inf) of
+    SomeInt when is_integer(SomeInt) -> ok;
+    _Other -> put(inf, 0)
+  end.
+
+process_stats()->
+  put(running, 0),
+  Processes = erlang:processes(),
+  io:format("NUM_PROCESSES IN THE SYSTEM: ~b~n", [length(Processes)]),
+  [print_status(P, {erlang:process_info(P, [status])})
+    || P <- erlang:processes()].
+
+print_status(Pid, {[{status,running}]}) ->
+  NumRunning = get(running) + 1,
+  io:format("~n~n~p * * * RUNNING PROCESS #~b!!! ~n", [Pid, NumRunning]),
+  put(running, NumRunning);
+print_status(Pid, {[{status, Other}]}) ->
+  NumProcs =
+  case get(Other) of
+    undefined -> 1;
+    Int when is_integer(Int) -> Int + 1
+  end,
+  put(Other, NumProcs),
+  io:format("~p [~p]...", [Pid, Other]).
+
+collect_responses(N, Finished, _Killed, Inf, N) ->
+  io:format("~n~b finished, ~b killed, ~b running forever~n", [Finished, Killed, Inf]).
+collect_responses(N, Finished, Killed, Inf, Total) ->
+  receive
+    {_HandlerPid, ok, _Duration} -> collect_responses(N+1, Finished+1, Killed, Inf, Total);
+    ?DONT_WAIT_FOR_ME ->
+      CurrInf = get(inf),
+      put(inf, CurrInf + 1),
+      collect_responses(N + 1, Finished, Killed, Inf+1, Total);
+    {_HandlerPid, killed} -> collect_responses(N+1, Finished, Killed+1, Total)
+  end.
+
+dispatch(Req, AcceptorPid) ->
+  spawn(?MODULE, process_request, [Req, AcceptorPid]).
+
+process_request(Req, AcceptorPid) ->
+  Self = self(),
+  HandlerPid = spawn(?MODULE, handle_request, [Req, Self]),
+  Start = os:system_time(millisecond),
+  receive
+    {HandlerPid, Resp} ->
+      End = os:system_time(millisecond),
+      Duration = End - Start,
+      io:format("...~p [~b]...", [Req, Duration]),
+      AcceptorPid ! {HandlerPid, Resp, Duration};
+    ?DONT_WAIT_FOR_ME -> AcceptorPid ! ?DONT_WAIT_FOR_ME;
+    _Other -> io:format("Received unexpected response~n")
+  after 5000 ->
+    io:format("xxxx ~p[K] xxxx", [Req]),
+    AcceptorPid ! {HandlerPid, killed},
+    exit(HandlerPid, timedout)
+  end.
+
+handle_request(Req, ParentPid) when is_integer(Req) ->
+  case Req rem ?BUG_MOD of
+    0 ->
+        io:format("~n**** ~p [INF]*****~n", [Req]),
+        ParentPid ! ?DONT_WAIT_FOR_ME,
+        handle_with_inf_loop_bug();
+    _Other ->
+        Resp = normal_handle(Req),
+        HandlerPid = self(),
+        ParentPid ! {HandlerPid, Resp}
+  end.
+
+
+normal_handle(Req) when is_integer(Req)->
+  count_to_1000_and_do_other_stuff_too(Req, 0).
+
+count_to_1000_and_do_other_stuff_too(_Req, 1000) -> ok;
+count_to_1000_and_do_other_stuff_too(Req, C) ->
+  case (Req rem 2) of
+    0 ->  binary:copy(<<Req/integer>>,300);
+    1 ->  binary:copy(<<(Req + 1)/integer>>,200)
+  end,
+  count_to_1000_and_do_other_stuff_too(Req, C+1).
+
+handle_with_inf_loop_bug()->
+    infinite_loop(0).
+
+infinite_loop(C) -> _A = binary:copy(<<1>>,200), _B = math:sqrt(1235), infinite_loop(C+1).
+
+
+%%%% TODO 1
+
+
+%%%  TODO 2
+%%%
+
+%%% TODO 3
+%%io:format("~n~b finished, ~b killed, ~b running forever~n", [Finished, Killed, Inf]).
